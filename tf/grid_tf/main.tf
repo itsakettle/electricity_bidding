4ce790cc0d317af6d84401d6f6d40c760dc5b5f9
Will Rogers <itsakettle@gmail.com>
2025-04-21 07:36:26 +0100

    remove old module

--- tf/grid_tf/main.tf
+++ tf/grid_tf/main.tf
@@ -1,13 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-  shared_credentials_files = ["~/.aws/credentials"]
-}
-
-module "tf_backend" {
-  source = "../electricity_bidding_tf_backend"
-  bucket_name = "electricity-bidding-tf-state"
-}
-
 module "extract_semo" {
   source = "./extract_semo"
   env = var.env
